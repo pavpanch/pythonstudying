91a85856b26550f45ff5e7b83c570d9ec61e89b5
Paul <82761429+pavpanch@users.noreply.github.com>
2024-07-13 20:03:22 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+DoneHomeworks = 12
+spent_ours = 1.5
+course_name = 'Python'
+time_to_each_work = spent_ours / DoneHomeworks
+
+print('Курс: ', course_name,', всего задач:', DoneHomeworks,', затрачено часов: ', spent_ours, ', среднее время выполнения ', time_to_each_work,' часа.')
