2cc12ebf21834f47e015d107b6a49c374d3e790a
Paul <82761429+pavpanch@users.noreply.github.com>
2024-07-13 18:34:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+# 1st program
+print('1st program')
+print(9 ** 0.5)
+print(9** 0.5 * 5)
+
+# 2nd program
+print('2nd program')
+print( 9.99 > 9.98 and 1000 != 1000.1)
+
+# 3d program
+a = 1234
+b = 5678
+a = a // 10 % 100
+b = b // 10 % 100
+print('3d program')
+print (a + b)
+
+#4th program
+a = 13.42
+b = 42.13
+c = a // 1
+e = b // 1
+d = a * 100 % (c * 100)
+f = b * 100 % (e * 100)
+print('4th program')
+print(c == f)
+print(e == d)
